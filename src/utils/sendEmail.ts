b8605cbbcc97c7afd1f620874b8fd316e933f80c
lengendarybu <145948119+lengendarybu@users.noreply.github.com>
2023-11-20 01:13:28 +0200

    Update sendEmail.ts

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const endpoint = "https://api.emailjs.com/api/v1.0/email/send";
-const service_id = "service_lscmbxg";
-const template_id = "template_dt113zd";
-const public_key = "f3JRwW3c7ZhKbn2h7";
+const service_id = "service_atld7r5";
+const template_id = "template_4hig63o";
+const public_key = "lNheQDcGvHdrBwzDM";
 
 const sendEmail = (values: any) => {
   const payload = {
